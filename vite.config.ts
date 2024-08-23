c55ba3af59b1d2cc0624f0b76e2f00dd5f93797a
Ishant Dahiya <ishantdahiya@gmail.com>
2024-08-23 22:58:17 +0000

    fix some build settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  define: {
+    'process.env': {
+      VITE_ENVIRONMENT: process.env.VITE_ENVIRONMENT
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
